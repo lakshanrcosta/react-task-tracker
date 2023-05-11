8750b9e35f203e9d3b94e77b26ec8c58e0b33ff4
Lakshan Costa <lakshanrcosta@gmail.com>
2023-05-11 15:34:01 +0300

    Style updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/header";
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
+      <div className={"container"}>
         <Header title="Task Tracker" />
       </div>
     );
